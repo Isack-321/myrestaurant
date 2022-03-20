4fa138368afd0c57ba0900259e638213de91ffb9
Isack <ituwei21@gmail.com>
2022-03-20 12:06:24 +0300

    fixed css typo

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,9 +11,9 @@ const AboutUs = () => (
     </div>
     <div className="app__aboutus-content flex-center">
       <div className="app__aboutus-content_about">
-        <h1 className="headtext_cormorant">About Us</h1>
+        <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon_img"/>
-        <p className="p__opensanss">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button type="button" className="custom__button"> know More</button>
       </div>
     </div>
